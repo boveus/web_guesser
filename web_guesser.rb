eb5a9da7544e607dc505f4f0af3ad0adb090215d
Brandon Stewart <boveus@gmail.com>
2017-08-06 13:53:28 -0600

    Add initial working version.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/' do
-  number = 1
-  guess = params["guess"]
-  message = "You guessed #{guess}\n"
-  if guess.to_i > number  + 4
-    message += "That is way too high."
-    erb :index, :locals => {:guess => guess, :message => message}
-  elsif guess.to_i < number - 4
-    message += "That is way too low."
-    erb :index, :locals => {:guess => guess, :message => message}
-  elsif guess.to_i < number
-    message += "That is too low."
-    erb :index, :locals => {:guess => guess, :message => message}
-  elsif guess.to_i > number
-    message += "That is too high."
-    erb :index, :locals => {:guess => guess, :message => message}
-  elsif guess.to_i == number
-    message += "That is correct!"
-    erb :index, :locals => {:guess => guess, :message => message}
-  end
+SECRET_NUMBER = rand(100)
 
+get '/' do
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:message => message}
+end
+
+def check_guess(guess)
+  message = "You guessed #{guess}. <br>"
+  if guess.to_i > SECRET_NUMBER  + 4
+    message += "That is way too high."
+  elsif guess.to_i < SECRET_NUMBER - 4
+    message += "That is way too low."
+  elsif guess.to_i < SECRET_NUMBER
+    message += "That is too low."
+  elsif guess.to_i > SECRET_NUMBER
+    message += "That is too high."
+  elsif guess.to_i == SECRET_NUMBER
+    message += "That is correct!"
+  end
 end
 
 # get '/' do
