6b45ff3358c3ac8ea36570218924e02368f94993
Brandon Stewart <boveus@gmail.com>
2017-08-06 13:24:27 -0600

    Add working way too high way too low.

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,28 +5,31 @@ get '/' do
   number = 1
   guess = params["guess"]
   message = "You guessed #{guess}\n"
-  if guess.to_i > number
-    message += "That is too high."
+  if guess.to_i > number  + 4
+    message += "That is way too high."
+    erb :index, :locals => {:guess => guess, :message => message}
+  elsif guess.to_i < number - 4
+    message += "That is way too low."
     erb :index, :locals => {:guess => guess, :message => message}
   elsif guess.to_i < number
     message += "That is too low."
     erb :index, :locals => {:guess => guess, :message => message}
+  elsif guess.to_i > number
+    message += "That is too high."
+    erb :index, :locals => {:guess => guess, :message => message}
+  elsif guess.to_i == number
+    message += "That is correct!"
+    erb :index, :locals => {:guess => guess, :message => message}
   end
+
 end
-#
-# if verbose == 'true'
-#      erb :newResultPage, :locals => {:result => result}
-#  elsif verbose == 'false'
-#      erb :resultPage, :locals => {:result => result}
-#  end
-# Change your ERB template to
-# output a variable named number:
+
+# get '/' do
+#   guess = params["guess"]
+#   message = check_guess(guess)
+#   erb :index, :locals => {:number => number, :message => message}
+# end
+# To make it work as expected, you can assign the secret number to a constant:
 #
 # 1
-# The SECRET NUMBER is <%= number %>
-
-# if the guess is too high
-#   set the message to "Too high!"
-# end
-# render the ERB template and pass in the number AND the message
-#
+# SECRET_NUMBER = rand(100)
